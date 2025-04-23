0d041052f83fad7bacda40aa34a83c556f325c17
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2025-04-22 23:52:14 -0700

    refactor to load gamesRank.js before call mongoose

--- server/app.js
+++ server/app.js
@@ -6,27 +6,16 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
 const gamesRankRoutes = require("./routes/gamesRank");
+const gameRoutes = require("./routes/game");
 
 const app = express();
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use(
-  session({
-    name: "session",
-    keys: [process.env.SESSION_SECRET || "secret-key"],
-    maxAge: 24 * 60 * 60 * 1000,
-  })
-);
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(session({}));
 
 app.use("/api", authRoutes);
 app.use("/api/gamesRank", gamesRankRoutes);
+app.use("/api/games", gameRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -36,7 +25,4 @@ mongoose
       console.log("Server running on http://localhost:3001")
     );
   })
-  .catch((err) => console.error(" MongoDB connection error:", err));
-
-const gameRoutes = require("./routes/game");
-app.use("/api/games", gameRoutes);
+  .catch((err) => console.error("MongoDB connection error:", err));
