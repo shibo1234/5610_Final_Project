eae7cdd33e3a45f35c300d26b07655d548c820bf
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2025-04-24 16:46:45 -0700

    update backend Render congfig

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
@@ -9,16 +11,36 @@ const gamesRankRoutes = require("./routes/gamesRank");
 const gameRoutes = require("./routes/game");
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    maxAge: 24 * 60 * 60 * 1000,
-    sameSite: "none",
-    cookie: { secure: false }, // TODO: Unsure to update this to true if deploying with HTTPS
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "none",
+      secure: true,
+    },
   })
 );
 
@@ -26,12 +48,14 @@ app.use("/api", authRoutes);
 app.use("/api/gamesRank", gamesRankRoutes);
 app.use("/api/games", gameRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("✅ MongoDB connected");
-    app.listen(3001, () =>
-      console.log("Server running on http://localhost:3001")
-    );
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+const frontendDir = path.join(__dirname, "..", "frontend", "dist");
+app.use(express.static(frontendDir));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendDir, "index.html"));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
